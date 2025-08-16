40bca5086b255e04d644e670bae67d84424c3dc8
vcostale <vcostale7@gmail.com>
2025-08-16 10:29:11 +0200

    SpectrumImageVisualizer changes with new implementations and callbacks. Requirements updated: plotly view instead of holoviews and bokeh

--- main.py
+++ main.py
@@ -3,3 +3,4 @@ from whateels import App
 if __name__ == "__main__":
     # This line is for running with 'python main.py'
     App(title='WhatEELS').run(port=5006)
+    
--- whateels/pages/home/MVC/view/eels_plots/__init__.py
+++ whateels/pages/home/MVC/view/eels_plots/__init__.py
@@ -2,8 +2,8 @@
 Visualization factories for different EELS data types.
 """
 
-from .spectrum_line_visualizer import SpectrumLineVisualizer
 from .spectrum_image_visualizer import SpectrumImageVisualizer
+from .spectrum_line_visualizer import SpectrumLineVisualizer
 
 __all__ = [
     'SpectrumLineVisualizer', 
